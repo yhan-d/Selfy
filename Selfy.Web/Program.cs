c502e5fccd9a971201d6333ad94d20830a551dba
elifboluk <101395811+elifboluk@users.noreply.github.com>
2022-05-17 15:09:35 +0300

    Layout oluşturuldu.

--- Selfy.Web/Program.cs
+++ Selfy.Web/Program.cs
@@ -1,6 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    //app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+//app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
