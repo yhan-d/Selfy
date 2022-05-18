03ea9e6c0965946e881ecf5474fe2e478f86ea74
semaadibelli <semanur.adibelli@gmail.com>
2022-05-18 12:33:27 +0300

    login

---
+++ Selfy.Web/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Selfy.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            return View();
+        }
+
+
+    }
+}
