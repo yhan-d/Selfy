104a90344a354edea337c3a6f6ebe3e4ee4f0671
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-05-20 10:53:50 +0300

    ApplicationUser ve ApplicationRole oluşturuldu.

---
+++ Selfy.Data/Identity/ApplicationRole.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Selfy.Data.Identity
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public string? Description { get; set; }
+
+        public ApplicationRole()
+        {
+        }
+
+        public ApplicationRole(string roleName, string description)
+            : base(roleName)
+        {
+            this.Description = description;
+        }
+    }
+}
---
+++ Selfy.Data/Identity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Selfy.Data.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public DateTime RegisterDate { get; set; } = DateTime.UtcNow;
+    }
+}
