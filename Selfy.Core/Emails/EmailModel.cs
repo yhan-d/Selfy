b31d3045f1e3bc05406f50dfc2288d5be8052699
semaadibelli <semanur.adibelli@gmail.com>
2022-05-20 11:21:17 +0300

    EmailModel ve Emailsettings oluşturuldu

---
+++ Selfy.Core/Emails/EmailModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Selfy.Core.Emails;
+public class EmailModel
+{
+    public string Adress { get; set; }
+    public string Name { get; set; }
+
+}
---
+++ Selfy.Core/Emails/MailModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Selfy.Core.Emails;
+public class MailModel
+{
+    public List<EmailModel> To { get; set; } = new();
+    public List<EmailModel> Cc { get; set; } = new();
+    public List<EmailModel> Bcc { get; set; } = new();
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public List<Stream> Attachs { get; set; }
+}
