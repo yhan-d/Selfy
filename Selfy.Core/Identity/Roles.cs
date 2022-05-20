18786257bfb52e0cc362603e84b83a628b78afdc
semaadibelli <semanur.adibelli@gmail.com>
2022-05-20 10:46:43 +0300

    Roller oluşturuldu.

---
+++ Selfy.Core/Identity/Roles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selfy.Core.Identity;
+
+public static class Roles
+{
+    public static readonly string Admin = "Admin";
+    public static readonly string User = "User";
+    public static readonly string Operator = "Operator";
+    public static readonly string Technician = "Technician";
+
+    public static List<string> RoleList = new List<string>()
+    {
+        Admin, User, Operator, Technician
+    };
+}
+
+
