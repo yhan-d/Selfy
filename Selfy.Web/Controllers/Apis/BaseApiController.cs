72b6efa217f123f3f353427c3d4ff185a161b5c6
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-06-14 11:53:53 +0300

    APIs has been created.

---
+++ Selfy.Web/Controllers/Apis/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Selfy.Web.Controllers.Apis
+{
+    [Route("api/[controller]/[action]/{id?}")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
---
+++ Selfy.Web/Controllers/Apis/RequestApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Selfy.Web.Controllers.Apis
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestApiController : ControllerBase
+    {
+    }
+}
