6a5d54fcbc7c14bb031e030e4e0aecfa758c3ae6
Yagizhan Demirci <52960386+yhan-d@users.noreply.github.com>
2022-06-15 14:13:08 +0300

    Repos has been created.

---
+++ Selfy.Business/Repositories/RequestRepo.cs
@@ -0,0 +1,13 @@
+﻿using Selfy.Business.Repositories.Abstracts.EntityFrameworkCore;
+using Selfy.Core.Entities;
+using Selfy.Data.EntityFramework;
+namespace Selfy.Business.Repositories
+{
+    public class RequestRepo : RepositoryBase<Request, int>
+    {
+        public RequestRepo(MyContext context) : base(context)
+        {
+
+        }
+    }
+}
